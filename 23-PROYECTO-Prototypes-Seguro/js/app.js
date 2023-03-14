f5d56150f7d3758513e3a47737b1c6f3efd36342
Robledogar <robledogar@gmail.com>
2023-03-14 10:57:23 +0100

    Comienzo del cálculo del seguro

--- 23-PROYECTO-Prototypes-Seguro/js/app.js
+++ 23-PROYECTO-Prototypes-Seguro/js/app.js
@@ -1,9 +1,55 @@
 
 // Constructores
-function seguro(marca, year, tipo) {
+function Seguro(marca, year, tipo) {
     this.marca = marca;
     this.year = year;
     this.tipo = tipo;
+}
+
+// Realiza la cotización con los datos
+Seguro.prototype.cotizarSeguro = function() {
+    /*
+    1 = Americano 1.15
+    2 = Asiatico 1.05
+    3 = Europeo 1.35
+    */
+
+    let cantidad;
+    const base = 2000; //para darle realismo
+
+    console.log(this.marca);
+    switch(this.marca) {
+
+        case '1':
+            cantidad = base * 1.15;
+            break;
+        case '2':
+            cantidad = base * 1.05;
+            break;
+        case '3':
+            cantidad = base * 1.35;
+            break;
+        default:
+            break;
+    }
+
+    // Leer el año
+    const diferencia = new Date().getFullYear() - this.year;
+    // Cada año que la diferencia es mayor el costo va a reducirse un 3%
+    cantidad -= ((diferencia * 3) * cantidad) / 100;
+
+    /*
+        Si el seguro es básico se multiplica por un 30% más
+        Si el seguro es completo se multiplica por un 50% más
+
+    */ 
+    if(this.tipo === 'basico') {
+        cantidad *= 1.30;
+    } else if(this.tipo === 'completo') {
+        cantidad *= 1.50;
+    }
+
+    return cantidad;
 }
 
 function UI() {};
@@ -86,9 +132,12 @@ function cotizarSeguro(e) {
     ui.mostrarMensaje('Cotizando...', 'exito');
 
     //Instanciar el seguro
-
+    const seguro = new Seguro(marca, year, tipo);
+    seguro.cotizarSeguro();
+    
 
     // Utilizar el prototype que va a cotizar
 
     
 }
+
