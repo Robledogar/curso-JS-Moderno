7fb84678a4b92249a6ebf68f40c8e85893bd276a
robledogar <robledogar@gmail.com>
2023-08-06 08:03:11 +0200

    Proyecto 32 finalizado

--- 32-PROYECTO-CRMIndexedDB/js/editarcliente.js
+++ 32-PROYECTO-CRMIndexedDB/js/editarcliente.js
@@ -1,16 +1,25 @@
 (function() {
 
     let DB;
+    let idCliente;
 
     const nombreInput = document.querySelector('#nombre');
+    const emailInput = document.querySelector('#email');
+    const telefonoInput = document.querySelector('#telefono');
+    const empresaInput = document.querySelector('#empresa');
+
+    const formulario = document.querySelector('#formulario');
 
     document.addEventListener('DOMContentLoaded', () => {
 
         conectarDB();
 
+        // Actualiza el registro
+        formulario.addEventListener('submit', actualizarCliente);
+
         //Verificar el ID de la URL
         const parametrosURL = new URLSearchParams(window.location.search);
-        const idCliente = parametrosURL.get('id');
+        idCliente = parametrosURL.get('id');
         
         if(idCliente) {
             setTimeout(() => {
@@ -20,6 +29,41 @@
         }
 
     });
+
+    function actualizarCliente(e) {
+        e.preventDefault();
+        if(nombreInput.value === '' || emailInput.value === '' || telefonoInput.value === '' || empresaInput.value === '') {
+            imprimirAlerta('Todos los campos son obligatorios', 'error');
+
+            return;
+        }
+
+        // Actualizar cliente
+        const clienteActualizado = {
+            nombre: nombreInput.value,
+            email: emailInput.value,
+            telefono: telefonoInput.value,
+            empresa: empresaInput.value,
+            id: Number(idCliente) //Lo combierte a numero
+        }
+
+        const transaction = DB.transaction(['crm'], 'readwrite');
+        const objectStore = transaction.objectStore('crm');
+
+        objectStore.put(clienteActualizado);
+
+        transaction.oncomplete = function() {
+            imprimirAlerta('Editado correctamente');
+
+            setTimeout(() => {
+                window.location.href = 'index.html'
+            }, 3000);
+        }
+
+        transaction.onerror = function() {
+            imprimirAlerta('Hubo un error', 'error');
+        }
+    }
 
     function obtenerCliente(id) {
         const transaction = DB.transaction(['crm'], 'readwrite');
@@ -41,9 +85,12 @@
     }
 
     function llenarFormulario(datosCliente) {
-        const {nombre} = datosCliente;
+        const {nombre, email, telefono, empresa} = datosCliente;
 
         nombreInput.value = nombre;
+        emailInput.value = email;
+        telefonoInput.value = telefono;
+        empresaInput.value = empresa;
 
     }
 
