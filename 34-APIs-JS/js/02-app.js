882fffe4e615bd40c00b586bd2a8cbf078f9097c
robledogar <robledogar@gmail.com>
2023-08-13 08:47:22 +0200

    Avanzando en el tema 34

--- 34-APIs-JS/js/02-app.js
+++ 34-APIs-JS/js/02-app.js
@@ -1,18 +1,29 @@
-// Previamente vimos getclientRect, que nos permitia identificar cuando un elemento estaba visible, existe otra API llamada Intersection Observer, veamos como utilizarla..
+// // Previamente vimos getclientRect, que nos permitia identificar cuando un elemento estaba visible, existe otra API llamada Intersection Observer, veamos como utilizarla..
+
+
+// document.addEventListener('DOMContentLoaded', () => {
+
+//     const observer = new IntersectionObserver( entries => {
+//         console.log(entries[0]);
+
+//         if(entries[0].isIntersecting) {
+//             console.log('Ya esta visible...')
+//         }
+//     })
+
+
+//     // El Elemento a observar...
+//     observer.observe(document.querySelector('.premium'));
+// });
+
 
 
 document.addEventListener('DOMContentLoaded', () => {
-
-    const observer = new IntersectionObserver( entries => {
-        console.log(entries[0]);
-
+    const observer = new IntersectionObserver(entries => {
         if(entries[0].isIntersecting) {
-            console.log('Ya esta visible...')
+            console.log('Ya es visible');
         }
-    })
+    });
 
-
-    // El Elemento a observar...
     observer.observe(document.querySelector('.premium'));
 });
-
--- 34-APIs-JS/js/03-app.js
+++ 34-APIs-JS/js/03-app.js
@@ -1,22 +1,34 @@
-// Detectar si tenemos o no RED...
+// // Detectar si tenemos o no RED...
 
-const enLinea = navigator.onLine;
+// const enLinea = navigator.onLine;
 
-console.log(enLinea);
+// console.log(enLinea);
 
-// Activar o desactivar... NETWORK 
+// // Activar o desactivar... NETWORK 
 
-// agregar un evento para offline y online
+// // agregar un evento para offline y online
 
-// Es muy común tener algo asi no? Uber cuando estas buscando tu transporte y si pierdes tu conexión te dice que no estas conectado. en javascript también es posible capturar algo asi
+// // Es muy común tener algo asi no? Uber cuando estas buscando tu transporte y si pierdes tu conexión te dice que no estas conectado. en javascript también es posible capturar algo asi
 
-window.addEventListener('online', updateNetState);
-window.addEventListener('offline', updateNetState);
-function updateNetState(e) {
-    if(navigator.onLine ) {
-        console.log('Si estas conectado')
+// window.addEventListener('online', updateNetState);
+// window.addEventListener('offline', updateNetState);
+// function updateNetState(e) {
+//     if(navigator.onLine ) {
+//         console.log('Si estas conectado')
+//     } else {
+//         console.log('Parece que has perdido tu conexión...')
+//     }
+// }
+
+
+
+window.addEventListener('online', actualizarEstado);
+window.addEventListener('offline', actualizarEstado);
+
+function actualizarEstado() {
+    if(navigator.online) {
+        console.log('Sí estás conectado')
     } else {
-        console.log('Parece que has perdido tu conexión...')
+        console.log('No estás conectado')
     }
 }
-
