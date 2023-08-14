979cb3436eaf0e965d852509118d9a812383b066
robledogar <robledogar@gmail.com>
2023-08-14 08:22:41 +0200

    Finalizando el tema 34

--- 34-APIs-JS/js/06-app.js
+++ 34-APIs-JS/js/06-app.js
@@ -1,46 +1,92 @@
+// const salida = document.querySelector('#salida');
+// const microfono = document.querySelector('#microfono');
+
+//  microfono.addEventListener('click', ejecutarSpeechAPI);
+
+// function ejecutarSpeechAPI() {
+
+//     const SpeechRecognition =  webkitSpeechRecognition;
+//     const recognition = new SpeechRecognition();
+
+//     // start recognition
+//     recognition.start();
+
+
+//     // This runs when the speech recognition service starts
+//     recognition.onstart = function() {
+//         salida.classList.add('mostrar');
+//         salida.innerHTML = "Escuchando...";
+//     };
+    
+//     recognition.onspeechend = function() {
+//         salida.innerHTML = "Se detuvo de ejecutar";
+//         recognition.stop();
+//     };
+  
+//     // This runs when the speech recognition service returns result
+//     recognition.onresult = function(e) {
+
+//         console.log(e.results);
+
+//         var transcript = e.results[0][0].transcript;
+//         var confidence = e.results[0][0].confidence;
+
+
+//         const speech = document.createElement('p');
+//         speech.innerHTML = `Grabado: ${transcript}`;
+
+//         const seguridad = document.createElement('p');
+//         seguridad.innerHTML =  `Seguridad:  ${ parseInt( confidence*100) } %`;
+
+//         salida.appendChild(speech);
+//         salida.appendChild(seguridad);
+//     };
+  
+
+// }
+
 const salida = document.querySelector('#salida');
 const microfono = document.querySelector('#microfono');
 
- microfono.addEventListener('click', ejecutarSpeechAPI);
+microfono.addEventListener('click', ejecutarSpeechAPI);
 
 function ejecutarSpeechAPI() {
-
-    const SpeechRecognition =  webkitSpeechRecognition;
+    const SpeechRecognition = webkitSpeechRecognition;
     const recognition = new SpeechRecognition();
 
-    // start recognition
     recognition.start();
 
-
-    // This runs when the speech recognition service starts
     recognition.onstart = function() {
         salida.classList.add('mostrar');
-        salida.innerHTML = "Escuchando...";
+        salida.textContent = 'Escuchando...';
+        console.log('Escuchando........')
     };
-    
+
     recognition.onspeechend = function() {
-        salida.innerHTML = "Se detuvo de ejecutar";
+        salida.textContent = 'Se dejó de grabar...';
         recognition.stop();
     };
-  
-    // This runs when the speech recognition service returns result
+
     recognition.onresult = function(e) {
+        console.log(e.results[0] [0]);
 
-        console.log(e.results);
-
-        var transcript = e.results[0][0].transcript;
-        var confidence = e.results[0][0].confidence;
-
+        const {confidence, transcript} = e.results[0][0];
 
         const speech = document.createElement('p');
-        speech.innerHTML = `Grabado: ${transcript}`;
+        speech.innerHTML = `Grabado ${transcript}`;
 
         const seguridad = document.createElement('p');
-        seguridad.innerHTML =  `Seguridad:  ${ parseInt( confidence*100) } %`;
+        seguridad.innerHTML = `Seguridad: ${parseInt(confidence * 100)}%`;
+
 
         salida.appendChild(speech);
         salida.appendChild(seguridad);
+
+        if(transcript == 'número') {
+            alert('Has dicho la palabra "número"');
+        }
+       
     };
-  
+
 
 }
