9d9d6f08e543217cc46942902ab2165033d472f5
robledogar <robledogar@gmail.com>
2023-09-22 01:29:32 +0200

    Primeros pasos en la aplicación

---
+++ 40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
@@ -0,0 +1,43 @@
+let cliente = {
+    mesa: '',
+    hora: '',
+    pedido: []
+};
+
+const btnGuardarCliente = document.querySelector('#guardar-cliente');
+btnGuardarCliente.addEventListener('click', guardarCliente);
+
+function guardarCliente() {
+    const mesa = document.querySelector('#mesa').value;
+    const hora = document.querySelector('#hora').value;
+
+    //Revisar si hay campos vacios
+
+    const camposVacios = [mesa, hora].some( campo=> campo === '');
+
+    if(camposVacios) {
+
+        //Verificar si ya hay una alerta
+        const existeAlerta = document.querySelector('.invalid-feedback');
+        if(!existeAlerta) {
+            const alerta = document.createElement('DIV');
+            alerta.classList.add('invalid-feedback', 'd-block', 'text-center');
+            alerta.textContent = 'Todos los campos son obligatorios';
+            document.querySelector('.modal-body form').appendChild(alerta);
+
+            //Elimina la alerta
+            setTimeout(() => {
+                alerta.remove();
+            },3000);
+        } 
+        return;
+    } 
+
+    //Asignar datos del formulario a cliente
+    cliente = {...cliente, mesa, hora,};
+    
+    //Ocultar modal
+    const modalFormulario = document.querySelector('#formulario');
+    const modalBootstrap = bootstrap.Modal.getInstance(modalFormulario);
+    modalBootstrap.hide();
+}
