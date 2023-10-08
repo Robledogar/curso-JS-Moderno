b5972932f60f15b3521b1df015b47eeac390b234
robledogar <robledogar@gmail.com>
2023-10-08 09:49:11 +0200

    Sólo falta la opción de editar cliente

---
+++ 44-PROYECTO-CRM-CRUD-REST/js/API.js
@@ -0,0 +1,50 @@
+const url = "http://localhost:4000/clientes";
+
+// Cuando se crea un nuevo cliente
+export const nuevoCliente = async cliente => {
+    try {
+        await fetch(url, {
+            method: 'POST', 
+            body: JSON.stringify(cliente), // data puede ser string o un objeto
+            headers:{
+              'Content-Type': 'application/json' // Y le decimos que los datos se enviaran como JSON
+            }
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// obtiene toso los clientes
+export const obtenerClientes = async () => {
+    try {
+        const resultado = await fetch(url);
+        const clientes = await resultado.json();
+        return clientes;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// Elimina un cliente
+export const eliminarCliente =async id => {
+    try {
+        await fetch(`${url}/${id}`, {
+            method: 'DELETE'
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+// Obtiene un cliente por su ID
+export const obtenerCliente =async id => {
+    try {
+        const resultado = await fetch(`${url}/${id}`);
+        const cliente = await resultado.json();
+        return cliente;
+    } catch (error) {
+        console.log(error);
+    }
+}
