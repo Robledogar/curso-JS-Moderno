3b46341c08c370675b33f463ab9e6646b8bd1413
robledogar <robledogar@gmail.com>
2023-10-11 18:46:49 +0200

    primeras configuraciones

---
+++ 47-ServiceWorkers-PWA/sw.js
@@ -0,0 +1,38 @@
+const nombreCache = 'apv-v1';
+
+const archivos = [
+    '/',
+    '/index.html',
+    '/css/bootstrap.css',
+    '/css/styles.css',
+    '/js/app.js',
+    '/js/apv.js'
+];
+
+
+// Cuando se instala el Service Worker
+self.addEventListener('install', e => {
+    console.log('Instalando el Service Worker');
+
+    e.waitUntil(
+        caches.open(nombreCache)
+            .then( cache => {
+                console.log('cacheando');
+                cache.addAll(archivos);
+            })
+    )
+
+    
+});
+
+// Activar el Service worker
+self.addEventListener('activate', e => {
+    console.log('Service Worker Activado');
+
+    console.log(e);
+})
+
+// Evento fetch para descargar archivos estáticos
+self.addEventListener('fetch', e => {
+    console.log('Fetch...', e);
+})
