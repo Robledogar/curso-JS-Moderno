c4b57d58fe17d7d18dfe112f45ba1f6f6aef39b0
robledogar <robledogar@gmail.com>
2023-10-12 19:15:29 +0200

    Fin del tema 47

--- 47-ServiceWorkers-PWA/sw.js
+++ 47-ServiceWorkers-PWA/sw.js
@@ -1,4 +1,4 @@
-const nombreCache = 'apv-v1';
+const nombreCache = 'apv-v3';
 
 const archivos = [
     '/',
@@ -10,29 +10,63 @@ const archivos = [
 ];
 
 
-// Cuando se instala el Service Worker
+
+// Instalar el service Worker, no se puede utilizar window, por lo tanto se utiliza self
 self.addEventListener('install', e => {
-    console.log('Instalando el Service Worker');
+    console.log('Instalado el Service Worker');
 
     e.waitUntil(
+
+        // Buen lugar para cachear - 
         caches.open(nombreCache)
-            .then( cache => {
-                console.log('cacheando');
+            .then( cache => { // Esta función es asincrona...
+                console.log('cacheando...');
                 cache.addAll(archivos);
             })
-    )
+    );
 
-    
+    // Revisar en App (Chrome) en Firefox en Almacenamiento
+
 });
 
-// Activar el Service worker
+// Activar el service worker...
 self.addEventListener('activate', e => {
     console.log('Service Worker Activado');
 
-    console.log(e);
-})
+    // Actualizar la PWA //
+    e.waitUntil(
+        caches.keys()
+            .then(keys => {
+                console.log(keys); 
 
-// Evento fetch para descargar archivos estáticos
+                return Promise.all(keys
+                        .filter(key => key !== nombreCache)
+                        .map(key => caches.delete(key)) // borrar los demas
+                    )
+            })
+    )
+});
+
+// Fetch events para el CSS, HTML, imagenes JS, y hasta llamados a fetch..
 self.addEventListener('fetch', e => {
-    console.log('Fetch...', e);
-})
+    console.log('Fetch.. ', e);
+
+    e.respondWith(
+        caches.match(e.request)
+            .then(respuestaCache => {
+                return respuestaCache || fetch(e.request);
+            })
+            .catch( () => caches.match('/error.html'))
+    );
+});
+
+// Al tener este fetch, un nombre de app, y una página de inicio, ya podremos agregar nuestra PWA a la página de inicio...
+
+
+// clICK EN LOS 3 PUNTOS
+
+//  More Tools -> Remote Devices y escribir.
+
+// http://127.0.0.1:5500/index.html
+
+// Presionar Open.
