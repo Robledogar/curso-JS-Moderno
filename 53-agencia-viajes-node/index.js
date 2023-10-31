0d727c65670bede33e11c8afa8ce8a060ca44e61
robledogar <robledogar@gmail.com>
2023-10-31 15:41:19 +0100

    PUG instalado

--- 53-agencia-viajes-node/index.js
+++ 53-agencia-viajes-node/index.js
@@ -1,28 +1,18 @@
 // const express = require('express'); Se eliminó en el curso porque es lo mismo que el import (aunque hay que poner type module en el package.json)
 import express from 'express';
+import router from './routes/index.js';
 
 const app = express();
 
 //Definir puerto
 const port = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {//req lo que enviamos y res lo que express nos responde
-    // res.send('Hola mundo')
-    // res.json({
-    //     id: 1,
-    //     nombre: 'Jose'
-    // });
 
-    res.send('Inicio');
-});
+//Agregar router
+app.use('/', router);
 
-app.get('/nosotros', (req, res) => {
-    res.send('Nosotros');
-});
-
-app.get('/contacto', (req, res) => {
-    res.send('Contacto');
-});
+//Habilitar PUG
+app.set('view engine', 'pug');
 
 
 app.listen(port, () => {
