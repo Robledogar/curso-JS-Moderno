0441624d234b4e9417560740f3f883f5e504fdf7
robledogar <robledogar@gmail.com>
2023-10-31 16:06:09 +0100

    Añadiendo la carpeta "public"

--- 53-agencia-viajes-node/routes/index.js
+++ 53-agencia-viajes-node/routes/index.js
@@ -3,15 +3,14 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {//req lo que enviamos y res lo que express nos responde
-    res.send('Inicio');
+    res.render('inicio');
 });
 
 router.get('/nosotros', (req, res) => {
-    res.send('Nosotros');
+
+    const viajes = 'Viaje a Alemania';
+    res.render('nosotros');
 });
 
-router.get('/contacto', (req, res) => {
-    res.send('Contacto');
-});
 
 export default router;
